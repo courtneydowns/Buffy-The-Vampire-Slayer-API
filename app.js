105f5835bd1de914f9ac27caec44ecfcd6e5bd96
Courtney Downs <courtneysdowns@gmail.com>
2021-08-28 17:23:29 -0400

    moving app.js to root level

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+let express = require("express");
+let app = express();
+let sequelize = require("./db");
+let episode = require("./controllers/episodeController");
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+sequelize.sync();
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(_dirname, "../client/index.html"));
+});
+
+app.use(express.json());
+
+app.use(require("cors")());
+app.use(require("body-parser").json());
+
+// app.use(require("./middleware/headers"));
+
+app.use("/episode", episode);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
