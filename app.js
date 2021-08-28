87a4b9dd58037ecf5aa47635a089e337e8fcad27
Courtney Downs <courtneysdowns@gmail.com>
2021-08-28 18:09:36 -0400

    changed episode path

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 let express = require("express");
 let app = express();
 let sequelize = require("./db");
-let episode = require("./server/controllers/episodeController");
+let episode = require("./controllers/episodeController");
 const port = process.env.PORT || 3000;
 const path = require("path");
 
